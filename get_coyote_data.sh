6e5c81e33626687b1e6875fe949b45199fc648be
Alec Moschetti <alecmoschetti@gmail.com>
2024-01-10 15:26:38 -0800

    Updated Python requirements check

--- get_coyote_data.sh
+++ get_coyote_data.sh
@@ -21,11 +21,13 @@ source "$VENV_DIR/bin/activate"
 # Check if requirements are installed
 REQUIREMENTS="requirements.txt"
 if [ -f "$REQUIREMENTS" ]; then
-    # Install requirements if not already satisfied
+    # Check if requirements are already satisfied
     python3 -m pip freeze > installed.txt
-    if ! diff -q "$REQUIREMENTS" installed.txt > /dev/null; then
-        echo "Installing required Python packages..."
-        python3 -m pip install -r "$REQUIREMENTS"
+
+    if diff -q "$REQUIREMENTS" installed.txt > /dev/null; then
+      python3 -m pip install --upgrade pip  # Ensure pip is up-to-date
+      echo "Installing required Python packages..."
+      python3 -m pip install -r "$REQUIREMENTS"
     fi
     rm installed.txt
 else
@@ -44,12 +46,10 @@ fi
 # Check if the file path is valid
 if [ -f "$FILE_PATH" ]; then
       python3 main.py "$FILE_PATH" 2>> "$ERROR_LOG" || log_error "main.py script execution failed."
+      python3 arc_gis_online.py 2>> "$ERROR_LOG" || log_error "arc_gis_online.py script execution failed."
 else
     echo "File does not exist or is not a regular file."
     exit 1
 fi
-
-# Run Arc Gis Online Python script
-python3 arc_gis_online.py 2>> "$ERROR_LOG" || log_error "arc_gis_online.py script execution failed."
 
 deactivate
